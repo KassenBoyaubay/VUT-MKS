796883ac457c537b999a82e1a601b6f38976d168
Kassen Boyaubay <kassen.boyaubay@nu.edu.kz>
2022-10-04 13:05:51 +0200

    2.3 Časovač SysTick a neblokující čekání

--- cv2/Src/main.c
+++ cv2/Src/main.c
@@ -23,30 +23,62 @@
 #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+volatile uint32_t Tick;
+
+void SysTick_Handler(void)
+ {
+ Tick++;
+ }
+
+void blikac(void)
+{
+static uint32_t delay;
+static uint32_t LED_TIME_BLINK = 300;
+
+if (Tick > delay + LED_TIME_BLINK) {
+	GPIOA->ODR ^= (1<<4);
+	delay = Tick;
+}
+}
+
 void EXTI0_1_IRQHandler(void)
-		  {
-		  if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
-		  EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
-		  GPIOB->ODR ^= (1<<0); // toggle
-		  }
-		 }
+  {
+  if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+  EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+  GPIOB->ODR ^= (1<<0); // toggle
+  }
+ }
 
 int main(void)
 {
+	SysTick_Config(8000);
+
+	static uint32_t old_s2;
+	uint32_t new_s2 = GPIOC->IDR & (1<<0);
+
+	if (old_s2 && !new_s2) { // falling edge
+	off_time = Tick + LED_TIME_SHORT;
+	GPIOB->BSRR = (1<<0);
+	}
+	old_s2 = new_s2;
+
+	static uint32_t off_time;
+	if (Tick > off_time) {
+	GPIOB->BRR = (1<<0);
+	}
+
 
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
-	 GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
-	 GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
-	 GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
-	 GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
+	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
+	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 
-	 RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
-	 SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
-	 EXTI->IMR |= EXTI_IMR_MR0; // mask
-	 EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
-	 NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
 
-
-
-	 while (1) {}
+	while (1) {}
 }
